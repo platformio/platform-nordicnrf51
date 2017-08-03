11cf3f2ad44cf355f5eee15b93efe23e5aa719b4
Valerii Koval <valeros@bigmir.net>
2017-08-03 22:23:59 +0300

    Update toolchain package version

--- platform.py
+++ platform.py
@@ -19,11 +19,3 @@ class Nordicnrf51Platform(PlatformBase):
 
     def is_embedded(self):
         return True
-
-    def configure_default_packages(self, variables, targets):
-        if "mbed" in variables.get("pioframework", []):
-                self.packages["toolchain-gccarmnoneeabi"][
-                    'version'] = ">=1.60301.0"
-
-        return PlatformBase.configure_default_packages(
-            self, variables, targets)
