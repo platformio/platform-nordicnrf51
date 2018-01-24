1f605339b4fedcb4ecf3bbc0899622f81add95e0
Ivan Kravets <me@ikravets.com>
2018-01-25 01:43:44 +0200

    Typo fix

--- platform.py
+++ platform.py
@@ -26,7 +26,9 @@ class Nordicnrf51Platform(PlatformBase):
                                             self.board_config(
                                                 variables.get("board")).get(
                                                     "upload.protocol", ""))
-            if upload_protocol != "nrfjprog":
+
+            if "tool-nrfjprog" in self.packages and \
+                    upload_protocol != "nrfjprog":
                 del self.packages["tool-nrfjprog"]
 
         return PlatformBase.configure_default_packages(self, variables,
